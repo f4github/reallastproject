e2fffd440443b1289840959eed431a24e5808442
sojaejeong <al.cheon03@gmail.com>
2019-04-27 16:28:59 +0900

    Revert "Revert "Revert "Revert "토요일 업뎃""""
    This reverts commit 841c0ee542d74427ed6e44b8153543fbce0baea2.

---
+++ matching/src/main/java/global/sesoc/project/ti/DAO/TI_CommentMapper.java
@@ -0,0 +1,14 @@
+package global.sesoc.project.ti.DAO;
+
+import java.util.ArrayList;
+
+import global.sesoc.project.ti.VO.TI_CommentVO;
+
+
+
+public interface TI_CommentMapper {
+	public int insert(TI_CommentVO vo);
+	public ArrayList<TI_CommentVO> list();
+	public void delete(int num);
+	public void edit(TI_CommentVO vo);
+}
