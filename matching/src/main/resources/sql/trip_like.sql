d33612335bfea21272b52d163579245fa0d30a8b
sojaejeong <al.cheon03@gmail.com>
2019-04-27 16:39:20 +0900

    Revert "Revert "Revert "Revert "Revert "Revert "토요일 업뎃""""""
    This reverts commit 07233446acca9420e5db95b058c15a181e8afe4f.

---
+++ matching/src/main/resources/sql/trip_like.sql
@@ -0,0 +1,29 @@
+create table trip_like (
+likenum	number	primary key,
+id	varchar2(20),
+title	varchar2(500),
+contentid	varchar2(200),
+contenttypeid	varchar2(200)
+);
+
+
+drop table trip_like;
+
+select * from trip_like;
+
+
+
+
+--���� �Ϸù�ȣ ������
+create sequence likenum_seq;
+
+likenum_seq.nextval
+
+
+
+
+
+
+
+
+
