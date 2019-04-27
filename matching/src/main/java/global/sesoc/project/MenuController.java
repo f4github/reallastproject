841c0ee542d74427ed6e44b8153543fbce0baea2
sojaejeong <al.cheon03@gmail.com>
2019-04-27 16:19:55 +0900

    Revert "Revert "Revert "토요일 업뎃"""
    This reverts commit 0598418fd7c53498ca9bfd4268d8f3f7e391236d.

--- matching/src/main/java/global/sesoc/project/MenuController.java
+++ matching/src/main/java/global/sesoc/project/MenuController.java
@@ -29,11 +29,6 @@ public class MenuController {
 		@Autowired
 		Travel_infoDAO tidao;
 
-		@RequestMapping(value = "/ttt", method = RequestMethod.GET)
-		public String Meqnu1()
-		{
-			return "ttt";
-		}
 		@RequestMapping(value = "/about-us", method = RequestMethod.GET)
 		public String Menu1()
 		{
